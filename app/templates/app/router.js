524bd8ae31ddf37c9a7fd993a1060fd48c647ea4
AJ Webb <aj.webb@vivint.com>
2013-11-13 15:30:20 -0700

    Added the ability to give an app a custom name as well as create an empty app shell

--- app/templates/app/router.js
+++ app/templates/app/router.js
@@ -1,8 +1,10 @@
 <%= _.classify(appname) %>.Router.map(function() {
+<% if(!empty) { %>
   this.route('about', { path: '/about' });
   this.route('contact', { path: '/contact_me' });
 
   this.resource('todos', function () {
     this.route('index', { path: '/' });
   });
+ <% } %>
 });
